a27921c63e1411b85bfd548519db4b65c3552dbd
Cooper Ross, UBCO <c.mross2907@gmail.com>
2025-02-09 20:25:33 -0800

    Throw Ball
    * Ball now moves with player

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,11 +4,15 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour{
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
     private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start(){
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall(){
@@ -16,6 +20,8 @@ public class BallController : MonoBehaviour{
             return;
         }
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward*force, ForceMode.Impulse);
     }
 }
